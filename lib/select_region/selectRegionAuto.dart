7c20590bb5a526366b2f4d26ec87d910e75a1b11
blackbox <dzzirima@gmail.com>
2022-04-19 03:11:04 +0200

    feat : left with navigatinf to specific screen

--- lib/select_region/selectRegionAuto.dart
+++ lib/select_region/selectRegionAuto.dart
@@ -2,6 +2,13 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:plant_app/navigation_screens/contacts.dart';
+import 'package:plant_app/navigation_screens/help_support.dart';
+import 'package:plant_app/screens/home/home_screen.dart';
+import 'package:plant_app/screens/region2/region2.dart';
+import 'package:plant_app/screens/region3/region3.dart';
+import 'package:plant_app/screens/region4/region4.dart';
+import 'package:plant_app/screens/region5/region5.dart';
 
 class SelectYourRegion extends StatefulWidget {
   const SelectYourRegion({Key key}) : super(key: key);
@@ -28,103 +35,213 @@ class _SelectYourRegionState extends State<SelectYourRegion> {
     'Midlands Province',
   ];
 
+  Future<bool> _onWillPop(context) async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Are you sure?'),
+            content: new Text('Do you want to exit Easy Farm'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: new Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Center(
-        child: ListView(
-          children: <Widget>[
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  "GafaaaaaS",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'lobster',
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+    return new WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: new Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text(
+            "Easy Farm",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
             ),
-            Container(
-              child: CircleAvatar(
-                radius: 150,
-                backgroundImage: AssetImage('assets/images/image_1.png'),
-                backgroundColor: Colors.blueGrey,
-                foregroundColor: Colors.white,
-              ),
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.95,
-                child: DropdownButtonFormField<String>(
-                  decoration: InputDecoration(
-                    alignLabelWithHint: true,
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(width: 3, color: Colors.blue)),
+          ),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Center(
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                    ),
                   ),
-                  value: selectedValue,
-                  icon: const Icon(Icons.arrow_downward),
-                  style: const TextStyle(color: Colors.black),
-                  onChanged: (String newValue) {
-                    //change the region you are in
-                    switch (newValue) {
-                      case "Bulawayo Province":
-                        currentRegion = 1;
-                        break;
-                      case "Harare Province":
-                        currentRegion = 2;
-                        break;
+                  Container(
+                    child: CircleAvatar(
+                      radius: 150,
+                      backgroundImage: AssetImage('assets/images/image_1.png'),
+                      backgroundColor: Colors.blueGrey,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      child: DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          alignLabelWithHint: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  BorderSide(width: 3, color: Colors.blue)),
+                        ),
+                        value: selectedValue,
+                        icon: const Icon(Icons.arrow_downward),
+                        style: const TextStyle(color: Colors.black),
+                        onChanged: (String newValue) {
+                          //change the region you are in
+                          switch (newValue) {
+                            case "Bulawayo Province":
+                              currentRegion = 1;
+                              break;
+                            case "Harare Province":
+                              currentRegion = 2;
+                              break;
 
-                      default:
-                        currentRegion = 0;
-                    }
+                            default:
+                              currentRegion = 0;
+                          }
 
-                    setState(() {
-                      selectedValue = newValue;
-                      currentRegion = currentRegion;
-                    });
-                  },
-                  items: regions.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: TextStyle(fontSize: 20),
+                          setState(() {
+                            selectedValue = newValue;
+                            currentRegion = currentRegion;
+                          });
+                        },
+                        items: regions
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          );
+                        }).toList(),
                       ),
-                    );
-                  }).toList(),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Center(
+                    child: Text(
+                      "Your region is  $currentRegion",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Center(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.50,
+                      child: CupertinoButton.filled(
+                        onPressed: () {
+                          log("$currentRegion");
+                          log(selectedValue);
+                        },
+                        child: const Text('Next'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              SizedBox(
+                height: 100.0,
+                child: const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                  child: Text(
+                    'EASY FARM',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: Text(
-                "Your region is  $currentRegion",
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.50,
-                child: CupertinoButton.filled(
-                  onPressed: () {
-                    log("helllo gafa");
-                    log(selectedValue);
-                  },
-                  child: const Text('Next'),
+              ListTile(
+                title: const Text('Home'),
+                leading: IconButton(
+                  icon: Icon(Icons.home),
                 ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
               ),
-            ),
-          ],
+              ListTile(
+                title: const Text('Contact Us'),
+                leading: IconButton(
+                  icon: Icon(Icons.contact_phone),
+                ),
+                onTap: () {
+                  _contacts(context);
+                  // Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Help & Support'),
+                leading: IconButton(
+                  icon: Icon(Icons.contact_support),
+                ),
+                onTap: () {
+                  _help_support(context);
+                },
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
+}
+
+home_screen(context) async {
+  Navigator.pushReplacementNamed(context, HomeScreen.id);
+}
+
+_region2(context) async {
+  Navigator.pushReplacementNamed(context, Region2.id);
+}
+
+_region3(context) async {
+  Navigator.pushReplacementNamed(context, Region3.id);
+}
+
+_region4(context) async {
+  Navigator.pushReplacementNamed(context, Region4.id);
+}
+
+_region5(context) async {
+  Navigator.pushReplacementNamed(context, Region5.id);
+}
+
+_contacts(context) async {
+  Navigator.pushReplacementNamed(context, Contacts.id);
+}
+
+_help_support(context) async {
+  Navigator.pushReplacementNamed(context, HelpSupport.id);
 }
